e3d83b675f668efc1fb52be125fa1e91beebec6e
Daniella Clemmensen <dani2610@live.dk>
2018-12-19 16:46:41 +0100

    CheckGetAllEducationIndex

--- LawHouse.UnitTest/XUnitTest.cs
+++ LawHouse.UnitTest/XUnitTest.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+ using LawHouse.UnitTest.Properties;
+using LawHouse.Shared.Entities;
 
 namespace LawHouse.UnitTest
 {
     [TestClass]
     public class XUnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        
+        public void CheckGetAllEducationIndex()
         {
+
+            const string sqlString = "select * from Efteruddannelse";
+            using (SqlConnection conn = new SqlConnection(Settings.Default.ConnString))
+            using (SqlCommand com = new SqlCommand(sqlString, conn))
+            {
+                using ()
+            }
         }
 
 
