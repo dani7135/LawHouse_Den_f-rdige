edb2694f6e1b4dac8d6e370e21a667acf70f43be
GitGodScrub <juliusbrandt@gmail.com>
2018-12-19 16:47:34 +0100

    changes unit test

--- LawHouse.UnitTest/XUnitTest.cs
+++ LawHouse.UnitTest/XUnitTest.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using LawHouse.DataAccess;
+using LawHouse.Shared.Entities;
+using LawHouse.Shared.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LawHouse.UnitTest
@@ -37,36 +42,57 @@ namespace LawHouse.UnitTest
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //40
+        [TestMethod]
+        public void CheckSQLDatabaseHandler_GetInstance() //Julius
+        {
+            IDatabase currentDatabaseInstance = SQLDatabaseHandler.Instance();
+            Assert.IsInstanceOfType(currentDatabaseInstance, typeof(IDatabase));
+        }
+        [TestMethod]
+        public void CheckEstablishConnectionToSQLDatabaseWithBuildinMethode() //Julius
+        {
+            string commandToRun = "";
+            SqlCommand com = new SqlCommand();
+            SQLDatabaseUtilities.RunSqlInsertCommand(commandToRun, com);
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void CheckEstablishConnectionToSQLDatabaseWithOwnString()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                "Data Source = den1.mssql8.gear.host;" +
+                "Initial Catalog = goodboysplus1v2;" +
+                "User ID = goodboysplus1v2;" +
+                "Password = Kr995B7kts;";
+            string whatVersion = null;
+            try
+            {
+                conn.Open();
+                whatVersion = conn.ServerVersion;
+            }
+            catch (Exception) { }
+            finally
+            {
+                
+                conn.Close();
+            }
+            Assert.IsNotNull(whatVersion);
+        }
+        [TestMethod]
+        public void CheckGetEmployeeService() //Julius
+        {
+            SQLDatabaseHandler currentDatabaseInstance = SQLDatabaseHandler.Instance();//kan være en idatabase evt
+            currentDatabaseInstance.GetAllEmployeeService();
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void CheckGetEmployeeServiceNotNull() //Julius
+        {
+            SQLDatabaseHandler currentDatabaseInstance = SQLDatabaseHandler.Instance();
+            List<EmployeeService> listToChek = currentDatabaseInstance.GetAllEmployeeService();
+            Assert.IsNotNull(listToChek);
+        }
     }
 }
